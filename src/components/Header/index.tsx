204ec3db3c5dab51ef1f041e633eb54d1322a8cc
wakenedo <a_nuner@outlook.com>
2021-09-22 11:02:53 -0300

    ActiveLink component
    Refactor of Header component , the effect of bold when the user has selected a page was turned into a component.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,22 @@
+import { ActiveLink } from '../ActiveLink';
 import styles from './styles.module.scss';
-import Link from 'next/link'
-import { useRouter } from 'next/router';
+
+
 
 export function Header() {
-    const { asPath } = useRouter();
 
-    console.log(asPath)
 
     return (
         <header className={styles.container}>
             <div className={styles.content} >
                 <img src="/logo.svg" alt="DevNews" />
                 <nav>
-                    <Link href='/'>
-                    <a className={asPath === '/' ? styles.active : ''}>Home</a>
-                    </Link>
-                    <Link href='/posts'>
-                    <a className={asPath === '/posts' ? styles.active : ''} >Posts</a>
-                    </Link>
+                    <ActiveLink href='/' activeClassName={styles.active}>
+                    <a >Home</a>
+                    </ActiveLink>
+                    <ActiveLink href='/posts' activeClassName={styles.active}>
+                    <a >Posts</a>
+                    </ActiveLink>
                 </nav>
             </div>
         </header>
