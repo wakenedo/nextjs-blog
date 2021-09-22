b7cae4b9173b94e4fa79297fa97f545164534fef
wakenedo <a_nuner@outlook.com>
2021-09-22 13:13:46 -0300

    CSR & SSR
    Exploring both uses of Nextjs, the CSR and SSR. For that i'm using json-server to use a mock api.
    
    To use the routing functions, and view the page working with the routes - use the command:
    
    npx json-server api.json -p 3333 -w

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,50 @@
-import styles from '../styles/home.module.scss';
+//import styles from '../styles/home.module.scss';
 
-export default function Home() {
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  posts: Post[];
+}
+
+//CSR - Client Side Rendering - Padrão React
+//export default function Home() {
+//  const [posts, setPosts] = useState<Post[]>([]);
+
+//  useEffect(() => {
+//    fetch('http://localhost:3333/posts').then((response) => {
+//      response.json().then((data) => {
+//        setPosts(data);
+//      });
+//    });
+//}, []);
+
+
+//SSR - Server Side Rendering - Usando o node que existe no Nextjs
+export default function Home({ posts } : HomeProps) {
   return (
-    <h1>
-      Olá <span>dev</span>
-    </h1>
-  )
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+  return {
+    props: {
+      posts,
+    },
+  }
 }
