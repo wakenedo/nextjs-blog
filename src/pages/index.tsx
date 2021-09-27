3c4893c8709617cfc6cee15018ced294e742b71e
wakenedo <a_nuner@outlook.com>
2021-09-27 14:59:09 -0300

    Home Page - Styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 //import styles from '../styles/home.module.scss';
-
+import Image from 'next/image'
 import { GetServerSideProps } from "next";
 import SEO from "../components/SEO";
+import styles from '../styles/home.module.scss';
 //import { useEffect, useState } from "react";
 
 interface Post {
@@ -29,15 +30,30 @@ interface HomeProps {
 //SSR - Server Side Rendering - Usando o node que existe no Nextjs
 export default function Home({ posts } : HomeProps) {
   return (
-    <div>
-      <SEO title='Home' />
-      <h1>Posts</h1>
+    <>
+      <SEO title='Dev News!' excludeTitleSuffix />
+      <main className={styles.content}>
+        <section className={styles.section}>
+          <span>Olá Dev!</span>
+          <h1>
+            Bem vindos & Bem vindas  <br />
+            ao <span>Dev</span> News!
+          </h1>
+          <p>
+            Um blog com conteúdos extremamentes <br />
+            <span>relevantes para o seu aprendizado</span>
+          </p>
+        </section>
+
+        <Image src='/home.svg' alt='Home Image' width='400' height='500'/>
+      </main>
+      {/*<h1>Posts</h1>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
-    </div>
+        </ul>*/}
+    </>
   );
 }
 
